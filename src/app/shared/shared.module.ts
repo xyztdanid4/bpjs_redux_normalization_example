99e48f88bd0b42932bc6ba397e675662b276ba5e
Toth Daniel <daniel.toth@interticket.hu>
2019-06-23 13:08:10 +0200

    Remove app button

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,10 +1,8 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-import { ButtonComponent } from './components/button/button.component';
 import { CalloutComponent } from './components/callout/callout.component';
 // Components
 const components = [
-  ButtonComponent,
   CalloutComponent
 ];
 
