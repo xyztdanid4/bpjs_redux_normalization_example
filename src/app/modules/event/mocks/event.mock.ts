39de11ff69bc96c62ee0502f6244f9b93cca79a0
Toth Daniel <daniel.toth@interticket.hu>
2019-07-16 11:39:46 +0200

    asd

--- src/app/modules/event/mocks/event.mock.ts
+++ src/app/modules/event/mocks/event.mock.ts
@@ -3,53 +3,53 @@ import { IEvent } from '../interfaces/event.interface';
 export const EVENT_MOCK: IEvent[] = [
   {
     id: 1,
-    name: 'event_name_1',
+    name: 'BudapestJs meetup July',
     place: {
       id: 1,
-      name: 'place_name_1',
-      address: 'place_address_1'
+      name: 'Interticket Dev HQ',
+      address: '1139, Budapest, Váci út 99'
     },
     pricings: [
       {
         id: 1,
-        name: 'pricing_name_1',
-        price: 1
+        name: 'Single event',
+        price: 100
       },
       {
         id: 2,
-        name: 'pricing_name_2',
-        price: 2
+        name: 'Multiple event',
+        price: 200
       },
       {
         id: 3,
-        name: 'pricing_name_3',
-        price: 3
+        name: 'Workshop event',
+        price: 300
       }
     ]
   },
   {
     id: 2,
-    name: 'event_name_2',
+    name: 'BudapestJs meetup August',
     place: {
       id: 2,
-      name: 'place_name_2',
-      address: 'place_address_2'
+      name: 'Supercharge HQ',
+      address: '1075, Budapest, Károly krt. 9'
     },
     pricings: [
       {
         id: 4,
-        name: 'pricing_name_4',
-        price: 4
+        name: 'Single event',
+        price: 1000
       },
       {
         id: 5,
-        name: 'pricing_name_5',
-        price: 5
+        name: 'Multiple event',
+        price: 2000
       },
       {
         id: 6,
-        name: 'pricing_name_6',
-        price: 6
+        name: 'Workshop event',
+        price: 30000
       }
     ]
   },
@@ -58,23 +58,23 @@ export const EVENT_MOCK: IEvent[] = [
     name: 'event_name_3',
     place: {
       id: 3,
-      name: 'place_name_3',
+      name: '1083, Budapest, Bókay János u. 44-46',
       address: 'place_address_3'
     },
     pricings: [
       {
         id: 7,
-        name: 'pricing_name_7',
+        name: 'Single event',
         price: 7
       },
       {
         id: 8,
-        name: 'pricing_name_8',
+        name: 'Multiple event',
         price: 8
       },
       {
         id: 9,
-        name: 'pricing_name_9',
+        name: 'Workshop event',
         price: 9
       }
     ]
